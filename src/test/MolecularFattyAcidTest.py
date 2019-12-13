53e958505f8e03e83906228e103c6c4565468d73
dominik.kopczynski <dominik.kopczynski@isas.de>
2019-12-13 08:54:37 +0100

    added unit test framework

--- src/test/MolecularFattyAcidTest.py
+++ src/test/MolecularFattyAcidTest.py
@@ -1,56 +1,59 @@
-from ..domain import *
+import unittest
 
-def main():
+from domain.LipidFaBondType import LipidFaBondType
+from domain.MolecularFattyAcid import MolecularFattyAcid
+from domain.LipidExceptions import *
+
+class MolecularFattyAcidTest(unittest.TestCase):
     
-    instanceZero = MolecularFattyAcid("FA1", 0, 2, 0, 0, LipidFaBondType.UNDEFINED, False)
-    assert 0 == instanceZero.num_double_bonds
-    
-    instanceOne = MolecularFattyAcid("FA1", 0, 2, 0, 1, LipidFaBondType.UNDEFINED, False)
-    assert 1 == instanceOne.num_double_bonds
+    def test_instanceZero(self):
+        instanceZero = MolecularFattyAcid("FA1", 0, 2, 0, 0, LipidFaBondType.UNDEFINED, False)
+        assert 0 == instanceZero.num_double_bonds
         
+        
+    def test_instanceOne(self):
+        instanceOne = MolecularFattyAcid("FA1", 0, 2, 0, 1, LipidFaBondType.UNDEFINED, False)
+        assert 1 == instanceOne.num_double_bonds
+            
 
-    try:
+    @unittest.expectedFailure
+    def test_wrong_bond_type(self):
         instanceZero = MolecularFattyAcid("FA1", 0, 2, 0, -1, LipidFaBondType.UNDEFINED, False)
-    except ConstraintViolationException:
-        pass
+            
+            
+    def test_name(self):
+        instance = MolecularFattyAcid("FAX", 0, 2, 0, 0, LipidFaBondType.UNDEFINED, False)
+        assert "FAX" == instance.name
+
+        
+    def test_position(self):
+        instance = MolecularFattyAcid("FAX", 1, 2, 0, 0, LipidFaBondType.UNDEFINED, False);
+        assert 1 == instance.position
         
 
-    instance = MolecularFattyAcid("FAX", 0, 2, 0, 0, LipidFaBondType.UNDEFINED, False)
-    assert "FAX" == instance.name
-
-    
-    instance = MolecularFattyAcid("FAX", 1, 2, 0, 0, LipidFaBondType.UNDEFINED, False);
-    assert 1 == instance.position
-    
-
-    try:
+    @unittest.expectedFailure
+    def test_wrong_carbon(self):
         instanceZero = MolecularFattyAcid("FA1", -2, 2, 0, 0, LipidFaBondType.UNDEFINED, False)
-    except ConstraintViolationException:
-        pass
 
 
-    instance = MolecularFattyAcid("FAX", 1, 2, 0, 0, LipidFaBondType.UNDEFINED, False)
-    assert 2 == instance.num_carbon
+    def test_carbon(self):
+        instance = MolecularFattyAcid("FAX", 1, 2, 0, 0, LipidFaBondType.UNDEFINED, False)
+        assert 2 == instance.num_carbon
 
 
-    try:
+    @unittest.expectedFailure
+    def test_wrong_db(self):
         instance = MolecularFattyAcid("FAX", 1, 1, 0, 0, LipidFaBondType.UNDEFINED, False)
-    except ConstraintViolationException:
-        pass
-    
+        
 
-    instance = MolecularFattyAcid("FAX", 1, 2, 1, 0, LipidFaBondType.UNDEFINED, False)
-    assert 1 == instance.num_hydroxy
+    def test_hydroxyl(self):
+        instance = MolecularFattyAcid("FAX", 1, 2, 1, 0, LipidFaBondType.UNDEFINED, False)
+        assert 1 == instance.num_hydroxy
 
 
-    try:
+    @unittest.expectedFailure
+    def test_wrong_hydroxyl(self):
         instance = MolecularFattyAcid("FAX", 1, 2, -1, 0, LipidFaBondType.UNDEFINED, False)
-    except ConstraintViolationException:
-        pass
     
-    print("test passed without problems")
-    
-    
-    
-if __name__ == "__main__":
-    main()
+if __name__ == '__main__':
+    unittest.main()
