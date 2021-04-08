ce02b655d5c10f3362303eea0e937a39dfda5d49
Dominik Kopczynski <dominik.kopczynski@isas.de>
2021-04-08 14:23:09 +0200

    adjusted the get_extended_class function

--- pygoslin/domain/LipidSpecies.py
+++ pygoslin/domain/LipidSpecies.py
@@ -55,9 +55,13 @@ class LipidSpecies:
     def get_extended_class(self):
         #special_case = self.lipid_class in self.special_cases if self.info != None and self.info.num_carbon > 0 else False
         special_case = self.lipid_category == LipidCategory.GP if self.info != None and self.info.num_carbon > 0 else False
-        if special_case and self.info.lipid_FA_bond_type in [LipidFaBondType.ETHER_PLASMANYL, LipidFaBondType.ETHER_UNSPECIFIED]: return self.head_group + "-O"
-        if special_case and self.info.lipid_FA_bond_type == LipidFaBondType.ETHER_PLASMENYL: return self.head_group + "-p"
-        return self.head_group
+        if special_case and self.info.lipid_FA_bond_type in [LipidFaBondType.ETHER_PLASMANYL, LipidFaBondType.ETHER_UNSPECIFIED]:
+            return all_lipids[self.lipid_class]["name"] + "-O"
+        
+        if special_case and self.info.lipid_FA_bond_type == LipidFaBondType.ETHER_PLASMENYL:
+            return all_lipids[self.lipid_class]["name"] + "-p"
+        
+        return all_lipids[self.lipid_class]["name"]
             
             
     def validate(self):
